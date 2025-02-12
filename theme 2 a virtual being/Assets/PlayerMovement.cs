a76f0e3b1fff72614af0ee61e4a8238963e739ce
jackireland2 <121182167+jackireland2@users.noreply.github.com>
2025-02-12 14:44:53 +0000

    update

--- theme 2 a virtual being/Assets/PlayerMovement.cs
+++ theme 2 a virtual being/Assets/PlayerMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //public float speed;
+    private Rigidbody2D rb;
+    Vector2 velocity;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        velocity = new Vector2(0.15f, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("a"))
+        {
+            rb.MovePosition(rb.position - velocity);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.MovePosition(rb.position + velocity);
+        }
     }
 }
