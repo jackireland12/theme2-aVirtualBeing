4ff18cdff3fe435dc7fd778a9f4aeae0408b641b
jackireland2 <121182167+jackireland2@users.noreply.github.com>
2025-02-15 00:58:00 +0000

    animation

--- theme 2 a virtual being/Assets/attackScript.cs
+++ theme 2 a virtual being/Assets/attackScript.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class attackScript : MonoBehaviour
 {
+
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        GetComponent<Animator>().speed = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E)) // Example trigger condition
+
+        {
+            Debug.Log("eiei");
+            //animator.Play("attack"); // Play the "Jump" animation
+            heavyAttack();
+        }
+    }
+
+    void heavyAttack()
+    {
+
+        animator.Play("attack",0,0);
     }
 }
